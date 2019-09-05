b5d37cc9e43a79c1f06eb8e5e59affa6f0853abf
Lucas Blancas <lablancas@gmail.com>
2019-09-05 14:38:08 -0500

    fix IE11 bug - add min width to SVG

--- src/theme.js
+++ src/theme.js
@@ -37,6 +37,7 @@ export const getTheme = (theme = base, override = {}) => {
       `,
       logo: `
         height: 100%;
+        min-width: 138px;
         padding: ${ theme.gutter }px 0;
 
         polygon, path {
