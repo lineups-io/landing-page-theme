bb7734a006911683863c8a92ebec9b065acdab24
Lucas Blancas <lablancas@gmail.com>
2019-10-18 10:51:56 -0500

    update colors, nav, and footer styles

--- theme.js
+++ theme.js
@@ -14,7 +14,7 @@ var headerFontFamily = _typography.default.headerFontFamily,
 var base = {
   gutter: 15,
   colors: {
-    primary: '#0085ca',
+    primary: '#f2b71f',
     secondary: '#ca4500',
     gray: {
       50: '#FAFAFA',
@@ -29,7 +29,7 @@ var base = {
       900: '#212121'
     },
     white: '#ffffff',
-    black: '#000000'
+    black: '#3b3c3a'
   },
   fonts: {
     header: headerFontFamily.join(','),
@@ -49,20 +49,20 @@ var getTheme = function getTheme(theme, override) {
 
   return (0, _extends2.default)({
     nav: {
-      container: "\n        background-color: " + theme.colors.primary + ";\n        color: " + theme.colors.white + ";\n      ",
-      logo: "\n        height: 100%;\n        padding: " + theme.gutter + "px 0;\n        margin-left: " + theme.gutter * -2 / 3 + "px;\n\n        polygon, path {\n          fill: " + theme.colors.white + ";\n        }\n      ",
-      link: "\n        text-transform: uppercase;\n        font-size: 0.8em;\n        letter-spacing: 1px;\n        font-weight: bold;\n        background-color: " + theme.colors.primary + ";\n        color: " + theme.colors.white + ";\n        border-width: 0 0 2px 0;\n        border-style: solid;\n        border-color: " + theme.colors.primary + ";\n        text-decoration: none;\n\n        &:hover, &[href]:hover {\n          color: " + theme.colors.white + ";\n          text-decoration: none;\n          border-color: " + theme.colors.white + ";\n        }\n\n        &[aria-haspopup]::after {\n          border-top-color: " + theme.colors.white + "\n        }\n      ",
+      container: "\n        background-color: " + theme.colors.white + ";\n        color: " + theme.colors.black + ";\n        border-width: 0 0 1px 0;\n        border-style: solid;\n        border-color: " + theme.colors.gray['200'] + ";\n      ",
+      logo: "\n        height: 100%;\n        padding: " + theme.gutter + "px 0;\n        margin-left: " + theme.gutter + "px;\n      ",
+      link: "\n        text-transform: uppercase;\n        font-size: 0.8em;\n        letter-spacing: 1px;\n        font-weight: bold;\n        background-color: " + theme.colors.white + ";\n        color: " + theme.colors.black + ";\n        border-width: 1px;\n        border-style: solid;\n        border-color: " + theme.colors.gray['200'] + ";\n        text-decoration: none;\n\n        &:hover, &[href]:hover {\n          color: " + theme.colors.primary + ";\n          text-decoration: none;\n        }\n      ",
       menu: {
-        container: "\n          background-color: " + theme.colors.primary + ";\n        ",
+        container: "\n          background-color: '#27283b';\n        ",
         hamburger: "\n          background-color: " + theme.colors.white + ";\n        ",
-        link: "\n          padding: " + theme.gutter * 2 / 3 + "px 0;\n          display: flex;\n          justify-content: center;\n          border-bottom-width: 3px;\n          font-size: 1.5em;\n        "
+        link: "\n          padding: " + theme.gutter * 2 / 3 + "px 0;\n          display: flex;\n          justify-content: center;\n          border-width: 0 0 3px 0;\n          border-color: " + theme.colors.primary + ";\n          color: " + theme.colors.white + ";\n          font-size: 1.5em;\n        "
       }
     },
     main: {
       container: "\n        background-color: " + theme.colors.white + "\n      ",
       title: "\n        margin: 0;\n        font-size: 2.5rem;\n        font-weight: 500;\n      ",
-      description: "\n        font-weight: lighter;\n        font-size: 1.25em;\n        color: " + theme.colors.gray['700'] + ";\n      ",
-      disclaimer: "\n        font-size: 0.9em;\n        color: " + theme.colors.gray['700'] + ";\n      "
+      description: "\n        font-weight: lighter;\n        font-size: 1.25em;\n        color: " + theme.colors.black + ";\n      ",
+      disclaimer: "\n        font-size: 0.9em;\n        color: " + theme.colors.black + ";\n      "
     },
     card: {
       body: "\n        background-color: " + theme.colors.white + ";\n        border-width: 2px;\n        border-style: solid;\n        border-color: " + theme.colors.gray['200'] + ";\n\n        margin: " + theme.gutter * -2 + "px " + theme.gutter + "px 0;\n        z-index: 1000;\n\n        @media (min-width: 768px) {\n          margin: 0;\n          border-width: 1px;\n        }\n      ",
@@ -72,40 +72,40 @@ var getTheme = function getTheme(theme, override) {
         hover: "\n\n        "
       },
       title: "\n        color: " + theme.colors.black + ";\n        font-size: 1.25em;\n        line-height: 1em;\n      ",
-      address: "\n        color: " + theme.colors.gray['700'] + ";\n        font-size: 1em;\n        font-weight: lighter;\n      ",
-      bedrooms: "\n        text-transform: uppercase;\n        color: " + theme.colors.gray['700'] + ";\n        font-size: 0.8em;\n        font-weight: lighter;\n      ",
+      address: "\n        color: " + theme.colors.black + ";\n        font-size: 1em;\n        font-weight: lighter;\n      ",
+      bedrooms: "\n        text-transform: uppercase;\n        color: " + theme.colors.black + ";\n        font-size: 0.8em;\n        font-weight: lighter;\n      ",
       amount: "\n        font-size: 1.1em;\n        color: " + theme.colors.black + ";\n      ",
       adLabel: "\n        color: " + theme.colors.black + ";\n        background-color: " + theme.colors.white + ";\n        box-shadow: 0 0 2px 2px " + theme.colors.gray['300'] + ";\n      ",
       button: "\n      "
     },
-    link: "\n      display: block;\n      color: " + theme.colors.gray['700'] + ";\n\n      border: 0;\n      background-color: transparent;\n      padding: 0;\n\n      &[href] {\n        cursor: pointer;\n        text-decoration: none;\n      }\n\n      &[href]:hover {\n        text-decoration: underline;\n        color: " + theme.colors.black + ";\n      }\n    ",
+    link: "\n      display: block;\n      color: " + theme.colors.black + ";\n\n      border: 0;\n      background-color: transparent;\n      padding: 0;\n\n      &[href] {\n        cursor: pointer;\n        text-decoration: none;\n      }\n\n      &[href]:hover {\n        text-decoration: underline;\n        color: " + theme.colors.black + ";\n      }\n    ",
     button: {
       primary: "\n        background-color: " + theme.colors.primary + ";\n        border-color: " + theme.colors.primary + ";\n        color: " + theme.colors.white + ";\n\n        &[href]:hover {\n          color: " + theme.colors.white + ";\n        }\n      ",
       danger: "\n        background-color: red;\n        border-color: red;\n        color: " + theme.colors.white + ";\n\n        &[href]:hover {\n          color: " + theme.colors.white + ";\n        }\n      ",
       outline: "\n        background-color: " + theme.colors.white + ";\n      ",
-      default: "\n        border-width: 1px;\n        border-style: solid;\n        border-color: " + theme.colors.gray['200'] + ";\n        background-color: " + theme.colors.gray['200'] + ";\n        color: " + theme.colors.gray['700'] + ";\n\n        &::after {\n          border-color: " + theme.colors.gray['700'] + ";\n        }\n      ",
+      default: "\n        border-width: 1px;\n        border-style: solid;\n        border-color: " + theme.colors.gray['200'] + ";\n        background-color: " + theme.colors.gray['200'] + ";\n        color: " + theme.colors.black + ";\n\n        &::after {\n          border-color: " + theme.colors.black + ";\n        }\n      ",
       link: "\n        border-color: " + theme.colors.white + ";\n        background-color: " + theme.colors.white + ";\n      ",
       disabled: "\n        color: " + theme.colors.gray['300'] + ";\n        background-color: " + theme.colors.gray['200'] + ";\n        border-color: " + theme.colors.gray['200'] + ";\n      "
     },
     dropdown: {
-      toggle: "\n        &::after {\n          content: \" \";\n          border-left: 5px solid transparent;\n          border-right: 5px solid transparent;\n          border-top-width: 5px;\n          border-top-style: solid;\n          border-top-color: " + theme.colors.gray['700'] + ";\n          margin-left: 10px;\n          margin-right: -5px;\n        }\n      ",
+      toggle: "\n        &::after {\n          content: \" \";\n          border-left: 5px solid transparent;\n          border-right: 5px solid transparent;\n          border-top-width: 5px;\n          border-top-style: solid;\n          border-top-color: " + theme.colors.black + ";\n          margin-left: 10px;\n          margin-right: -5px;\n        }\n      ",
       menu: "\n        background-color: " + theme.colors.white + ";\n        border-width: 1px;\n        border-style: solid;\n        border-color: " + theme.colors.gray['200'] + ";\n      ",
-      header: "\n        color: " + theme.colors.gray['700'] + ";\n        font-weight: bold;\n      ",
-      item: "\n        color: " + theme.colors.gray['700'] + ";\n        font-size: 0.9em;\n\n        &:hover, &[href]:hover {\n          color: " + theme.colors.black + ";\n          background-color: " + theme.colors.gray['200'] + ";\n          text-decoration: none;\n        }\n\n        &[aria-current='page'], &.active {\n          color: " + theme.colors.white + ";\n          background-color: " + theme.colors.primary + ";\n          &[href]:hover {\n            color: " + theme.colors.white + ";\n            background-color: " + theme.colors.primary + ";\n          }\n        }\n      "
+      header: "\n        color: " + theme.colors.black + ";\n        font-weight: bold;\n      ",
+      item: "\n        color: " + theme.colors.black + ";\n        font-size: 0.9em;\n\n        &:hover, &[href]:hover {\n          color: " + theme.colors.black + ";\n          background-color: " + theme.colors.gray['200'] + ";\n          text-decoration: none;\n        }\n\n        &[aria-current='page'], &.active {\n          color: " + theme.colors.white + ";\n          background-color: " + theme.colors.primary + ";\n          &[href]:hover {\n            color: " + theme.colors.white + ";\n            background-color: " + theme.colors.primary + ";\n          }\n        }\n      "
     },
-    well: "\n      background-color: " + theme.colors.gray['200'] + ";\n      color: " + theme.colors.gray['700'] + ";\n\n      h2 {\n        color: " + theme.colors.black + ";\n      }\n\n      a {\n        display: inline;\n        color: " + theme.colors.gray['700'] + ";\n        box-shadow: " + theme.colors.primary + " 0px -2px 0px inset;\n        text-decoration: none;\n        transition: background 0.15s cubic-bezier(0.33, 0.66, 0.66, 1) 0s;\n        border-width: 0 0 2px 0;\n        border-style: solid;\n        border-color: " + theme.colors.primary + ";\n      }\n\n      a[href]:hover {\n        text-decoration: none;\n      }\n    ",
+    well: "\n      background-color: " + theme.colors.gray['200'] + ";\n      color: " + theme.colors.black + ";\n\n      h2 {\n        color: " + theme.colors.black + ";\n      }\n\n      a {\n        display: inline;\n        color: " + theme.colors.black + ";\n        box-shadow: " + theme.colors.primary + " 0px -2px 0px inset;\n        text-decoration: none;\n        transition: background 0.15s cubic-bezier(0.33, 0.66, 0.66, 1) 0s;\n        border-width: 0 0 2px 0;\n        border-style: solid;\n        border-color: " + theme.colors.primary + ";\n      }\n\n      a[href]:hover {\n        text-decoration: none;\n      }\n    ",
     related: {
-      header: "\n        font-size: 1em;\n        font-weight: bold;\n        color: " + theme.colors.gray['700'] + ";\n      ",
-      link: "\n        font-size: 0.9em;\n        color: " + theme.colors.gray['700'] + ";\n\n        &[href]:hover {\n          color: " + theme.colors.black + ";\n          text-decoration: underline;\n        }\n      "
+      header: "\n        font-size: 1em;\n        font-weight: bold;\n        color: " + theme.colors.black + ";\n      ",
+      link: "\n        font-size: 0.9em;\n        color: " + theme.colors.black + ";\n\n        &[href]:hover {\n          color: " + theme.colors.black + ";\n          text-decoration: underline;\n        }\n      "
     },
     footer: {
-      container: "\n        background-color: " + theme.colors.white + ";\n        color: " + theme.colors.gray['700'] + ";\n        border-width: 1px 0 0 0;\n        border-style: solid;\n        border-color: " + theme.colors.gray['200'] + ";\n      ",
+      container: "\n        background-color: " + theme.colors.primary + ";\n        color: " + theme.colors.black + ";\n        border-width: 1px 0 0 0;\n        border-style: solid;\n        border-color: " + theme.colors.primary + ";\n      ",
       header: "\n      ",
-      link: "\n        color: " + theme.colors.gray['700'] + ";\n\n        svg * {\n          fill: " + theme.colors.gray['700'] + ";\n        }\n\n        svg:hover * {\n          fill: " + theme.colors.black + ";\n        }\n\n        & {\n          cursor: pointer;\n        }\n\n        &[href]:hover {\n          text-decoration: underline;\n          color: " + theme.colors.black + ";\n        }\n      "
+      link: "\n        color: " + theme.colors.black + ";\n\n        svg * {\n          fill: " + theme.colors.black + ";\n        }\n\n        svg:hover * {\n          fill: " + theme.colors.black + ";\n        }\n\n        & {\n          cursor: pointer;\n        }\n\n        &[href]:hover {\n          text-decoration: underline;\n          color: " + theme.colors.black + ";\n        }\n      "
     },
-    label: "\n      color: " + theme.colors.gray['700'] + ";\n      font-size: .9em;\n    ",
+    label: "\n      color: " + theme.colors.black + ";\n      font-size: .9em;\n    ",
     slider: {
-      text: "\n        color: " + theme.colors.gray['700'] + ";\n      ",
+      text: "\n        color: " + theme.colors.black + ";\n      ",
       background: "\n        background-color: " + theme.colors.gray['200'] + ";\n      ",
       handle: "\n        background: " + theme.colors.white + ";\n        border-color: " + theme.colors.gray['200'] + ";\n      ",
       progressBar: "\n        background-color: " + theme.colors.primary + ";\n      "
