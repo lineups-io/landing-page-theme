52e2abb4d2bfa24d29a727d1050591b446acdf1b
Lucas Blancas <lablancas@gmail.com>
2019-10-23 10:21:31 -0500

    add top padding for menu container

--- theme.js
+++ theme.js
@@ -53,7 +53,7 @@ var getTheme = function getTheme(theme, override) {
       logo: "\n        height: 100%;\n        padding: " + theme.gutter + "px 0;\n      ",
       link: "\n        text-transform: none;\n        font-size: 1.2em;\n        letter-spacing: 1px;\n        font-weight: normal;\n        background-color: " + theme.colors.white + ";\n        color: " + theme.colors.black + ";\n        border-width: 1px;\n        border-style: solid;\n        border-color: " + theme.colors.gray['200'] + ";\n        text-decoration: none;\n\n        &:hover, &[href]:hover {\n          color: " + theme.colors.primary + ";\n          text-decoration: none;\n        }\n      ",
       menu: {
-        container: "\n          background-color: #27283b;\n        ",
+        container: "\n          background-color: #27283b;\n          padding-top: 100px;\n        ",
         hamburger: {
           height: 3,
           width: 20,
