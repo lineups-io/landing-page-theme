72062c2f6cbab69f9cc09fd8f4bf8bb0726430de
Lucas Blancas <lablancas@gmail.com>
2019-11-05 10:03:23 -0600

    update fonts

--- typography.js
+++ typography.js
@@ -3,9 +3,9 @@
 exports.__esModule = true;
 exports.default = void 0;
 var _default = {
-  headerFontFamily: ['Work Sans', 'sans-serif'],
-  bodyFontFamily: ['Work Sans', 'sans-serif'],
-  bodyWeight: 500,
+  headerFontFamily: ['Playfair Display', 'serif'],
+  bodyFontFamily: ['Roboto', 'sans-serif'],
+  bodyWeight: 400,
   overrideThemeStyles: function overrideThemeStyles(vr, options, styles) {
     return {
       'h1,h2,h3,h4,h5,h6': {
