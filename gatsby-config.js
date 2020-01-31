9700fb9d19975858bf4461e01caae59e025c4880
Lucas Blancas <lablancas@gmail.com>
2020-01-31 17:27:18 -0600

    convert to a gatsby site that uses a theme

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+const activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || 'development'
+
+console.log('[landing-page-site] ' + activeEnv)
+
+require('dotenv').config({
+  path: `${ __dirname }/.env.${ activeEnv }`,
+})
+
+module.exports = {
+  "plugins": [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: process.env.TITLE,
+        short_name: process.env.TITLE,
+        start_url: '/',
+        background_color: '#FFFFFF',
+        theme_color: '#E51F3B',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.svg', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-theme-landing-page',
+  ]
+}
