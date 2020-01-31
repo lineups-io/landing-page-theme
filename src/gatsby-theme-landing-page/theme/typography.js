875a64158f26e6f0b47f628f41751977b2473fed
Lucas Blancas <lablancas@gmail.com>
2020-01-31 17:27:41 -0600

    use gatsby theme shadowing

--- src/gatsby-theme-landing-page/theme/typography.js
+++ src/gatsby-theme-landing-page/theme/typography.js
@@ -2,7 +2,7 @@ export default {
   headerFontFamily: ['Source Sans Pro', 'sans-serif'],
   bodyFontFamily: ['Oxygen', 'sans-serif'],
   bodyWeight: 400,
-  overrideThemeStyles: function overrideThemeStyles(vr, options, styles) {
+  overrideThemeStyles: (vr, options, styles) => {
     return {
       'h1,h2,h3,h4,h5,h6': {
         marginBottom: vr.rhythm(1 / 3),
