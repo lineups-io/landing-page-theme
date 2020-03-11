a52db82fe98d2eda580c3f96330380e99269e55f
Lucas Blancas <lablancas@gmail.com>
2020-03-11 13:20:36 -0500

    feat: 🎸 add home page

---
+++ src/components/Home/Hero/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import GatsbyImage from 'gatsby-image'
+
+import Hero from './styled'
+import Section from '../Section'
+import { Container } from 'gatsby-theme-core/src/components/Layout/styled'
+import Icon from 'gatsby-theme-core/src/components/Icon'
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query getHeroData {
+      hero: file(relativePath: { eq: "index/hero.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1600 cropFocus: CENTER) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return <Section id='hero' dark noPadding>
+    <GatsbyImage fluid={data.hero.childImageSharp.fluid} />
+    <Container>
+      <Hero>
+        <Hero.Header>Life Made Simple is:</Hero.Header>
+        <Hero.Subheader>Reserving your new home online.</Hero.Subheader>
+        <Hero.Link href='#markets'>
+            View Our Locations <Icon icon='Arrow' />
+        </Hero.Link>
+      </Hero>
+    </Container>
+  </Section>
+}
---
+++ src/components/Home/Section.js
@@ -0,0 +1,118 @@
+import styled from 'styled-components'
+import { lighten } from 'polished'
+
+import Link from 'gatsby-theme-core/src/components/Link'
+import { Col } from 'gatsby-theme-core/src/components/Layout/styled'
+
+const Section = styled.div`
+  position: relative;
+  background-color: ${ props =>
+    props.bg || (props.dark
+      ? props.theme.colors.gray['700']
+      : props.theme.colors.white
+    ) };
+  color: ${ props => props.theme.colors[props.dark ? 'white' : 'black'] };
+  font-size: .8em;
+
+  a, button {
+    border-color:
+      ${ props => props.theme.colors[props.dark ? 'white' : 'black'] };
+    color: ${ props => props.theme.colors[props.dark ? 'white' : 'black'] };
+  }
+
+  a:hover, button:hover {
+    color: ${ props => props.theme.colors[props.dark ? 'white' : 'black'] };
+    text-decoration: none;
+  }
+
+  svg * {
+    fill: ${ props => props.theme.colors[props.dark ? 'white' : 'black'] };
+  }
+
+  h2::after {
+    background-color:
+    ${ props => lighten(props.dark ? 0.2 : 0, props.theme.colors.primary) };
+  }
+
+  h3 {
+    color:
+    ${ props => lighten(props.dark ? 0.2 : 0, props.theme.colors.primary) };
+  }
+
+  text-align: center;
+  padding-top: ${ props => props.noPadding ? 0 : 15 }px;
+  padding-bottom: ${ props => props.noPadding ? 0 : 15 }px;
+
+  @media (min-width: 576px) {
+    font-size: 1em;
+    padding-top: ${ props => props.noPadding ? 0 : 60 }px;
+    padding-bottom: ${ props => props.noPadding ? 0 : 60 }px;
+  }
+
+`
+
+Section.Aside = styled(Col)`
+  text-align: left;
+
+  @media (min-width: 576px) {
+    flex: 0 0 280px;
+    max-width: 280px;
+  }
+`
+
+Section.Header = styled.h2`
+  font-family: 'Gilda Display', serif;
+  font-size: 2em;
+  line-height: 1.25em;
+  font-weight: 400;
+
+  &::after {
+    width: 24px;
+    height: 2px;
+    content: ' ';
+    display: ${ props => props.noBorder ? 'none' : 'block' };
+    margin-top: 15px;
+  }
+`
+
+Section.Subheader = styled.h3`
+  font-family: 'Avenir', sans-serif;
+  font-weight: 600;
+  font-size: 1em;
+  line-height: 1.5em;
+  text-transform: uppercase;
+  margin-bottom: 25px;
+`
+
+Section.Body = styled(Col)`
+  text-align: left;
+
+  @media (min-width: 576px) {
+    flex: 1;
+  }
+`
+
+Section.Text = styled.p`
+  font-family: 'Gilda Display', serif;
+  font-size: 1.4em;
+  line-height: 1.6em;
+  font-weight: 200;
+`
+
+Section.Link = styled(Link)`
+  display: inline-block;
+  margin-top: 30px;
+  padding: 20px 30px;
+  border-width: 1px;
+  border-style: solid;
+  border: 1px solid;
+  transition: background-color 442ms ease;
+  line-height: 1em;
+  font-size: 1.1em;
+
+  > svg {
+    margin-left: 25px;
+  }
+`
+
+export default Section
