67e0f525154d3939f20440abaf40129bd019f786
Lucas Blancas <lablancas@gmail.com>
2020-04-09 18:28:26 -0500

    refactor: 💡 set body weight to 400

--- src/gatsby-theme-core/components/Theme/typography.js
+++ src/gatsby-theme-core/components/Theme/typography.js
@@ -1,7 +1,7 @@
 export default {
   headerFontFamily: ['Work Sans', 'sans-serif'],
   bodyFontFamily: ['Work Sans', 'sans-serif'],
-  bodyWeight: 500,
+  bodyWeight: 400,
   overrideThemeStyles: function overrideThemeStyles(vr, options, styles) {
     return {
       'h1,h2,h3,h4,h5,h6': {
