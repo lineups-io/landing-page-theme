736386e121d90ab814beddb04fe821db087b7f6f
Lucas Blancas <lablancas@gmail.com>
2020-04-10 08:50:50 -0500

    refactor: 💡 disable gatsby theme apartment page

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,6 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-theme-core',
-    'gatsby-theme-apartment-page',
     'gatsby-theme-landing-page',
   ]
 }
