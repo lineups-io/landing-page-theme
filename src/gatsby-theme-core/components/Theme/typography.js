dd106c32e91f10d01b8c3f9b2a622ae5a25053be
Lucas Blancas <lablancas@gmail.com>
2020-04-11 20:01:00 -0500

    fix: 🐛 faulty merge

--- src/gatsby-theme-core/components/Theme/typography.js
+++ src/gatsby-theme-core/components/Theme/typography.js
@@ -1,7 +1,7 @@
 export default {
-  headerFontFamily: ['Work Sans', 'sans-serif'],
-  bodyFontFamily: ['Work Sans', 'sans-serif'],
-  bodyWeight: 400,
+  headerFontFamily: ['proxima-nova', 'serif'],
+  bodyFontFamily: ['Catamaran', 'sans-serif'],
+  bodyWeight: 500,
   overrideThemeStyles: function overrideThemeStyles(vr, options, styles) {
     return {
       'h1,h2,h3,h4,h5,h6': {
