2476675ded8d8c6777ee803a71fe423ef20b1f72
Lucas Blancas <lablancas@gmail.com>
2020-05-20 17:06:27 -0500

    refactor: 💡 do not use Page Context from gatsby-theme-core

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,17 @@ import { graphql } from 'gatsby'
 import Layout from 'gatsby-theme-atomic-design/src/templates/Blank'
 
 import Helmet from 'gatsby-theme-core/src/components/Helmet'
-import { PageContext } from 'gatsby-theme-core/src/components/PageContext'
 import Home from '../components/Home'
 
 const Page = props => {
   const { site } = props.data.lineups
 
-  return <PageContext.Provider value={{ site }}>
+  return <>
       <Helmet title='Texas Apartments for Rent' />
       <Layout {...site}>
           <Home />
       </Layout>
-  </PageContext.Provider>
+  </>
 }
 
 export const query = graphql`
--- src/pages/residents.js
+++ src/pages/residents.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import Helmet from 'gatsby-theme-core/src/components/Helmet'
-import { PageContext } from 'gatsby-theme-core/src/components/PageContext'
 import Layout from 'gatsby-theme-atomic-design/src/templates/Blank'
 
 import ApartmentPicker from '../components/ApartmentPicker'
@@ -46,7 +45,7 @@ const Page = props => {
   const { lineups, ...images } = props.data
   const { site, apartments } = lineups
 
-  return <PageContext.Provider value={{ site }}>
+  return <>
       <Helmet title='Rockstar Resident Portal - Pay Rent, Submit Service Request & More' />
       <Layout {...site}>
         <ApartmentPicker
@@ -56,7 +55,7 @@ const Page = props => {
           {...images}
         />
       </Layout>
-  </PageContext.Provider>
+  </>
 }
 
 export const query = graphql`
