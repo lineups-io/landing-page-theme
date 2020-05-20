4d72a6f798ee7355ce8bfb44e1ebafc52023c52f
Lucas Blancas <lablancas@gmail.com>
2020-05-20 17:15:12 -0500

    fix: 🐛 style of Apartment Picker Card- image, h3, text, buttons

--- src/components/ApartmentPicker/index.js
+++ src/components/ApartmentPicker/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import GatsbyImage from 'gatsby-image'
 
-import Card from 'gatsby-theme-landing-page/src/templates/LandingPage/Card/styled'
 import Container from 'gatsby-theme-atomic-design/src/atoms/Container'
 import Row from 'gatsby-theme-atomic-design/src/atoms/Row'
 import Dropdown from 'gatsby-theme-atomic-design/src/molecules/Dropdown'
 import MenuItem from 'gatsby-theme-atomic-design/src/atoms/Dropdown/MenuItem'
 import Link from 'gatsby-theme-atomic-design/src/atoms/Link'
+
+import CardBody from 'gatsby-theme-atomic-design/src/atoms/ApartmentCard/Body'
+import CardTitle from 'gatsby-theme-atomic-design/src/atoms/ApartmentCard/Heading'
+import CardButtonGroup from 'gatsby-theme-atomic-design/src/atoms/ApartmentCard/ButtonGroup'
+import Button from 'gatsby-theme-atomic-design/src/atoms/Button/Button'
 
 import {
   DropdownRow,
@@ -14,6 +18,8 @@ import {
   CardContainer,
   Header,
   Description,
+  CardText,
+  CardImage
 } from './styled.js'
 
 const ApartmentPicker = ({ cards, h1, apartments = [], ...images }) => {
@@ -25,29 +31,32 @@ const ApartmentPicker = ({ cards, h1, apartments = [], ...images }) => {
     <DropdownRow>
       <DropdownContainer>
         <Dropdown selected='Select Apartment'>
-            {apartments.filter(apartment => apartment.url)
-              .map((apartment, i) =>
+          {apartments.filter(apartment => apartment.url)
+            .map((apartment, i) =>
               <MenuItem key={i}>
                 <Link href={apartment.url}>{apartment.name}</Link>
               </MenuItem>
-              )
-            }
+            )
+          }
         </Dropdown>
       </DropdownContainer>
     </DropdownRow>
     <Row>
       {cards.map((card, i) => <CardContainer key={i}>
-        {images[card.image] && images[card.image].childImageSharp
-          ? <GatsbyImage fluid={images[card.image].childImageSharp.fluid} />
-          : null}
-        <Card.Body>
-          <Card.Title>{card.title}</Card.Title>
-          <Card.Address>{card.body}</Card.Address>
-          <Card.Fill />
-          {card.link
-            ? <Card.Button type='primary' {...card.link} />
+        <CardImage>
+          {images[card.image] && images[card.image].childImageSharp
+            ? <GatsbyImage fluid={images[card.image].childImageSharp.fluid} />
             : null}
-        </Card.Body>
+        </CardImage>
+        <CardBody>
+          <CardTitle as='h3'>{card.title}</CardTitle>
+          <CardText>{card.body}</CardText>
+          <CardButtonGroup>
+            {card.link
+              ? <Button type='secondary' {...card.link} />
+              : null}
+          </CardButtonGroup>
+        </CardBody>
       </CardContainer>)}
     </Row>
   </Container>
