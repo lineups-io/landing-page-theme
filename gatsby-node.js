638636ade0959a82bf54bb1d6bd035b6035736fa
Lucas Blancas <lablancas@gmail.com>
2020-05-25 15:06:37 -0500

    fix: 🐛 remove deprecated core-js@2

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,1 @@
 // gatsby-node.js
-exports.onCreateWebpackConfig = function onCreateWebpackConfig({ actions, stage, getConfig }) {
-  const config = getConfig()
-  if (stage === 'develop') {
-    config.entry.commons.unshift(require.resolve('core-js'))
-    actions.replaceWebpackConfig(config)
-  }
-}
