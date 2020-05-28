6d6d1f40f046b63b7a62ed1e07ac9ecd94126b2c
Lucas Blancas <lablancas@gmail.com>
2020-05-28 09:08:46 -0500

    fix: 🐛 use public gatsby-plugin-google-tagmanager

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
-        ids: process.env.GOOGLE_TAG_MANAGER_ID.split(','),
+        id: process.env.GOOGLE_TAG_MANAGER_ID,
       },
     },
     {
