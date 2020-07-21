78b9a17e944206eccb42932f0f60a018ded46ccc
Lucas Blancas <lablancas@gmail.com>
2020-07-21 15:00:15 -0500

    refactor: 💡 adjust section h4 on locations page

---
+++ src/gatsby-theme-atomic-design/templates/Locations/Section.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import Section from 'gatsby-theme-atomic-design/src/templates/Locations/Section'
+
+const CustomSection = styled(Section)`
+  h4 {
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 0.9em;
+  }
+`
+
+export default CustomSection
