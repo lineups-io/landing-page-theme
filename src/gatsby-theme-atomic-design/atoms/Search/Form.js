e1ce2427c024ebc12ec6fffbcfc11869608b92ee
Lucas Blancas <lablancas@gmail.com>
2020-08-06 16:19:57 -0500

    refactor: 💡 change page bg color to gray 200

---
+++ src/gatsby-theme-atomic-design/atoms/Search/Form.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+import Form from 'gatsby-theme-atomic-design/src/atoms/Search/Form'
+
+const CustomForm = styled(Form).attrs({
+  type: 'gray200',
+})`
+
+`
+
+export default CustomForm
