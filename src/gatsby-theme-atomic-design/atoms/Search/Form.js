506f6b9afee5985f2c830d2091fcfbddfe093b78
Lucas Blancas <lablancas@gmail.com>
2020-08-07 11:30:10 -0500

    refactor: 💡 set page bg color to white

--- src/gatsby-theme-atomic-design/atoms/Search/Form.js
+++ src/gatsby-theme-atomic-design/atoms/Search/Form.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import Form from 'gatsby-theme-atomic-design/src/atoms/Search/Form'
 
 const CustomForm = styled(Form).attrs({
-  type: 'gray200',
+  type: 'white',
 })`
 
 `
--- src/gatsby-theme-atomic-design/templates/Blank/PageBackground.js
+++ src/gatsby-theme-atomic-design/templates/Blank/PageBackground.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import PageBackground from 'gatsby-theme-atomic-design/src/templates/Blank/PageBackground'
 
 const CustomPageBackground = styled(PageBackground).attrs({
-  type: 'gray200',
+  type: 'white',
 })`
 `
 
