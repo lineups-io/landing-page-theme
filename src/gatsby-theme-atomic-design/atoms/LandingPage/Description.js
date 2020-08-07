8f9db5e9f9b58360b13bb67bcd77d8e086093f2e
Lucas Blancas <lablancas@gmail.com>
2020-08-07 11:30:41 -0500

    refactor: 💡 set copy bg color to gray 200

---
+++ src/gatsby-theme-atomic-design/atoms/LandingPage/Description.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import Description from 'gatsby-theme-atomic-design/src/atoms/LandingPage/Description'
+
+const CustomDescription = styled(Description).attrs({
+  type: 'gray200',
+})`
+`
+
+export default CustomDescription
