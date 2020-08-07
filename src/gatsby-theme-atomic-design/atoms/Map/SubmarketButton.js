3f46dccb6eb5d1c776f15753eeb7a8a95757b343
Lucas Blancas <lablancas@gmail.com>
2020-08-07 11:31:08 -0500

    refactor: 💡 change color for map icons

--- src/gatsby-theme-atomic-design/atoms/Map/SubmarketButton.js
+++ src/gatsby-theme-atomic-design/atoms/Map/SubmarketButton.js
@@ -3,10 +3,10 @@ import styled from 'styled-components'
 import SubmarketButton from 'gatsby-theme-atomic-design/src/atoms/Map/SubmarketButton'
 
 const CustomSubmarketButton = styled(SubmarketButton)`
-  color: ${props => props.theme.fontColorWithBackground.gold};
+  color: ${props => props.theme.fontColorWithBackground.tertiary};
 
   svg {
-    color: ${props => props.theme.colors.gold};
+    color: ${props => props.theme.colors.tertiary};
   }
 `
 
