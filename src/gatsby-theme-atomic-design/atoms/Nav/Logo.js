6ff8218efe06f2221663eef921da91462609a3f2
Lucas Blancas <lablancas@gmail.com>
2020-08-17 11:13:42 -0500

    refactor: 💡 adjust spacing around nav logo

---
+++ src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+import Logo from 'gatsby-theme-atomic-design/src/atoms/Nav/Logo'
+
+const CustomLogo = styled(Logo)`
+  height: 34px;
+  width: 130px;
+  margin: 0;
+`
+
+export default CustomLogo
