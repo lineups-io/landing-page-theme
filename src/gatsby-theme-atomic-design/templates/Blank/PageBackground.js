f55ff2cec5f816d7db7ce66dea3866c9fd48892f
Lucas Blancas <lablancas@gmail.com>
2020-08-17 16:59:58 -0500

    refactor: 💡 change page bg color to white

---
+++ src/gatsby-theme-atomic-design/templates/Blank/PageBackground.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+import PageBackground from 'gatsby-theme-atomic-design/src/templates/Blank/PageBackground'
+
+const CustomPageBackground = styled(PageBackground).attrs({
+  type: 'white',
+})`
+
+`
+
+export default CustomPageBackground
