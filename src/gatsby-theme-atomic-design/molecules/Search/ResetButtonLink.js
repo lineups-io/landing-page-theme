0b50a130db0546ea2b5e2a99510aeddda16fc971
Lucas Blancas <lablancas@gmail.com>
2020-08-17 17:03:54 -0500

    fix: 🐛 change reset button bg color to white

---
+++ src/gatsby-theme-atomic-design/molecules/Search/ResetButtonLink.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+import ResetButtonLink from 'gatsby-theme-atomic-design/src/molecules/Search/ResetButtonLink'
+
+export default props => <ResetButtonLink {...props} type='white' />
