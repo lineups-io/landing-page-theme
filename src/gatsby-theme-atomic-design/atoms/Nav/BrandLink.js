32b6024fc4e765db43967a20669abc7e77826641
Lucas Blancas <lablancas@gmail.com>
2020-08-17 17:02:33 -0500

    refactor: 💡 adjust nav logo size and spacing

---
+++ src/gatsby-theme-atomic-design/atoms/Nav/BrandLink.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import BrandLink from 'gatsby-theme-atomic-design/src/atoms/Nav/BrandLink'
+
+const CustomBrandLink = styled(BrandLink)`
+  padding: 15px 0;
+`
+
+export default CustomBrandLink
