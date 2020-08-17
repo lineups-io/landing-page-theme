35619d7a86a84f776dfaeb77d09f51e6873b2efd
Lucas Blancas <lablancas@gmail.com>
2020-08-17 16:58:51 -0500

    refactor: 💡 set h1 to 2.5x base font size

--- src/gatsby-theme-atomic-design/atoms/Typography/options.js
+++ src/gatsby-theme-atomic-design/atoms/Typography/options.js
@@ -2,6 +2,7 @@ export default {
   headerFontFamily: ['utopia-std', 'serif'],
   bodyFontFamily: ['proxima-nova', 'sans-serif'],
   bodyWeight: 500,
+  scaleRatio: 2.5,
   overrideThemeStyles: function overrideThemeStyles(vr, options, styles) {
     return {
       'h1,h2,h3,h4,h5,h6': {
