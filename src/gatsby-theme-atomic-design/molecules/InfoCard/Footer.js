edd722a8c0a6aa8440eccc357ced7bc8e3c381e9
Lucas Blancas <lablancas@gmail.com>
2020-10-05 16:35:37 -0500

    fix: 🐛 use absolute path for contact us link

--- src/gatsby-theme-atomic-design/molecules/InfoCard/Footer.js
+++ src/gatsby-theme-atomic-design/molecules/InfoCard/Footer.js
@@ -7,7 +7,7 @@ import Link from 'gatsby-theme-atomic-design/src/atoms/Link'
 export default ({ onlineLeasingUrl }) => {
   return (
     <Footer>
-      <Button type='secondary' invert as={Link} target='_blank' href='contact-us'>
+      <Button type='secondary' invert as={Link} target='_blank' href='/contact-us/'>
         Contact Us
       </Button>
       <Button type='secondary' as={Link} href={onlineLeasingUrl}>Check Availability</Button>
