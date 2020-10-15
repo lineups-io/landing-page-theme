502f5fea5dd0639e60b450cba834e2a3fbb875a3
Lucas Blancas <lablancas@gmail.com>
2020-10-15 13:46:19 -0500

    refactor: 💡 update nav logo

--- src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
+++ src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
@@ -2,10 +2,15 @@ import styled from 'styled-components'
 
 import Logo from 'gatsby-theme-atomic-design/src/atoms/Nav/Logo'
 
-const CustomLogo = styled(Logo)`
-  margin: 0;
-  width: 104px;
-  height: 35px;
+import logoUrl from './logo.jpeg'
+
+const CustomLogo = styled(Logo).attrs({
+  as: 'img',
+  src: logoUrl,
+})`
+  margin: 0 0 0 -15px;
+  width: 171px;
+  height: 64px;
 `
 
 export default CustomLogo
