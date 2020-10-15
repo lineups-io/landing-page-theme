4d774eb22eb34133809862cf255f1a4e341c48ff
Lucas Blancas <lablancas@gmail.com>
2020-10-15 13:47:34 -0500

    refactor: 💡 update navbar bg and font colors

--- src/gatsby-theme-atomic-design/atoms/Nav/Background.js
+++ src/gatsby-theme-atomic-design/atoms/Nav/Background.js
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 import Background from 'gatsby-theme-atomic-design/src/atoms/Nav/Background'
 
 const CustomBackground = styled(Background).attrs({
-  type: 'white',
+  type: 'primary',
 })`
-  border-bottom: 1px solid ${ props => props.theme.colors.gray200 };
+  border-bottom: 1px solid ${ props => props.theme.colors.primary };
 `
 
 export default CustomBackground
