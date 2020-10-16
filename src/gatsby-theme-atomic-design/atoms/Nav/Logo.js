86cd545b8b4848f5db27d43d678846c0d27adf74
Lucas Blancas <lablancas@gmail.com>
2020-10-16 12:58:55 -0500

    refactor: 💡 update logo and theme colors

--- src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
+++ src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
@@ -2,14 +2,14 @@ import styled from 'styled-components'
 
 import Logo from 'gatsby-theme-atomic-design/src/atoms/Nav/Logo'
 
-import logoUrl from './logo.jpeg'
+import logoUrl from './logo.jpg'
 
 const CustomLogo = styled(Logo).attrs({
   as: 'img',
   src: logoUrl,
 })`
-  margin: 0 0 0 -15px;
-  width: 171px;
+  margin: 0 0 0 -10px;
+  width: 110px;
   height: 64px;
 `
 
