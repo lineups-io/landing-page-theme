1904e6de5cf12604fcbb33f23e1cdb1f4be2b09b
Lucas Blancas <lablancas@gmail.com>
2020-10-16 12:21:28 -0500

    refactor: 💡 adjust font sizes: h1 and apartment name on card

---
+++ src/gatsby-theme-atomic-design/atoms/ApartmentCardVersion2/Heading.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import Heading from 'gatsby-theme-atomic-design/src/atoms/ApartmentCardVersion2/Heading'
+
+const CustomCardHeading = styled(Heading)`
+  font-size: 1.1rem;
+`
+
+export default CustomCardHeading
