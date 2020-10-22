c57c20d9f75db828f8aaa479193bd52f84e5edff
Lucas Blancas <lablancas@gmail.com>
2020-10-22 13:22:52 -0500

    feat: 🎸 work in progress - widget routing and slides

---
+++ src/templates/WidgetPage/NavLeft.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import NavButton from 'gatsby-theme-atomic-design/src/molecules/MultipleChoiceQuestion/NavButton'
+
+const NavLeft = props => <NavButton direction='left' text='Back' {...props} />
+
+export default NavLeft
