906952dafe9d4be591946a85988ab69167d86c13
Lucas Blancas <lablancas@gmail.com>
2020-11-24 10:19:20 -0600

    fix: 🐛 use PNG for logo. svg was too large for netlify deploy

---
+++ src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import logoUrl from './logo.png'
+
+const Logo = styled.img.attrs({
+  src: logoUrl,
+})`
+  margin: 8px 0;
+  height: 48px;
+  width: 178px;
+`
+
+export default Logo
