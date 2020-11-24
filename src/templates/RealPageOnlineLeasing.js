b17d5208494491a552a9b5e9319be1814e1049c7
Lucas Blancas <lablancas@gmail.com>
2020-11-24 11:50:09 -0600

    refactor: 💡 remove unnecessary styles

--- src/templates/RealPageOnlineLeasing.js
+++ src/templates/RealPageOnlineLeasing.js
@@ -8,14 +8,6 @@ import Helmet from 'gatsby-theme-atomic-design/src/organisms/Helmet'
 import { toQueryString } from 'gatsby-theme-atomic-design/src/helpers'
 
 const Container = styled.div`
-  @media screen and (max-width: 480px) {
-    width:100%;
-
-    body {
-      padding:0;
-      margin:0;
-    }
-  }
 `
 
 const Page = ({ pageContext, data, location }) => {
