37d6d1a13f46f2b30268dc538ab71e3d1761d6b0
Lucas Blancas <lablancas@gmail.com>
2020-11-25 17:23:03 -0600

    refactor: 💡 use SVG for header and footer logo

--- src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
+++ src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
 
-import logoUrl from './logo.jpg'
+import Icon from 'gatsby-theme-atomic-design/src/atoms/Icon'
 
-const Logo = styled.img.attrs({
-  src: logoUrl,
+const Logo = styled(Icon).attrs({
+  icon: 'HeaderLogo',
 })`
   margin: 8px 0;
   height: 48px;
-  width: 178px;
+  width: 189px;
 `
 
 export default Logo
