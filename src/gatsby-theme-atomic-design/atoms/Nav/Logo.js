3be3d1d1f17bc46b06395ea3f122a54c451e6b40
Lucas Blancas <lablancas@gmail.com>
2020-12-01 10:47:20 -0600

    fix: 🐛 reduce nav logo size on small devices to prevent wrappin

--- src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
+++ src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
@@ -5,9 +5,21 @@ import Icon from 'gatsby-theme-atomic-design/src/atoms/Icon'
 const Logo = styled(Icon).attrs({
   icon: 'HeaderLogo',
 })`
-  margin: 8px 0;
-  height: 48px;
-  width: 189px;
+  margin: 17px 0;
+  height: 30px;
+  width: 120px;
+
+  @media (min-width: 375px) {
+    margin: 12.5px 0;
+    height: 39px;
+    width: 155px;
+  }
+
+  @media (min-width: 425px) {
+    margin: 8px 0;
+    height: 48px;
+    width: 189px;
+  }
 `
 
 export default Logo
