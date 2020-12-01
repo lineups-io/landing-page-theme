141e0695983ef969ba2decb67522b50ee313d0f3
Lucas Blancas <lablancas@gmail.com>
2020-12-01 12:47:20 -0600

    refactor: 💡 update logo and responsive sizes

--- src/gatsby-theme-atomic-design/atoms/Footer/Logo.js
+++ src/gatsby-theme-atomic-design/atoms/Footer/Logo.js
@@ -5,8 +5,8 @@ import Icon from 'gatsby-theme-atomic-design/src/atoms/Icon'
 const Logo = styled(Icon).attrs({
   icon: 'FooterLogo',
 })`
-  margin: 8px 0;
-  height: 48px;
+  margin: 7px 0;
+  height: 50px;
   width: 189px;
 `
 
