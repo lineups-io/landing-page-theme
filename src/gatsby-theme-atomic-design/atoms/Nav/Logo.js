1eb936cdc99e00133f114596acb6a5f943ae2776
Lucas Blancas <lablancas@gmail.com>
2020-12-01 12:47:20 -0600

    refactor: 💡 update logo and responsive sizes

--- src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
+++ src/gatsby-theme-atomic-design/atoms/Nav/Logo.js
@@ -5,19 +5,19 @@ import Icon from 'gatsby-theme-atomic-design/src/atoms/Icon'
 const Logo = styled(Icon).attrs({
   icon: 'HeaderLogo',
 })`
-  margin: 17px 0;
-  height: 30px;
-  width: 120px;
+  margin: 15px 0;
+  height: 34px;
+  width: 130px;
 
   @media (min-width: 375px) {
-    margin: 12.5px 0;
-    height: 39px;
-    width: 155px;
+    margin: 11px 0;
+    height: 42px;
+    width: 160px;
   }
 
   @media (min-width: 425px) {
-    margin: 8px 0;
-    height: 48px;
+    margin: 7px 0;
+    height: 50px;
     width: 189px;
   }
 `
