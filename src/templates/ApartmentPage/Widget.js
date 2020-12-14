d30cf335167f71dbd954e049075b53776a42852c
Lucas Blancas <lablancas@gmail.com>
2020-12-14 12:41:52 -0600

    refactor: 💡 add 9:16 iframe container using css

--- src/templates/ApartmentPage/Widget.js
+++ src/templates/ApartmentPage/Widget.js
@@ -7,20 +7,12 @@ import {
   Iframe,
 } from './Widget.styled'
 
-// TODO: add support for landscape layout
-const getDimensions = width => ({
-  width,
-  height: width * 16 / 9,
-})
-
 const Widget = ({ _id, title, intro }) => {
   const [open, setOpen] = useState(false)
-  const [width, setWidth] = useState(320)
 
   const iframe = {
     title,
     src: `/widgets/${ _id }`,
-    ...getDimensions(width),
   }
 
   // TODO: set bubble text and bg color from widget styles
@@ -32,14 +24,7 @@ const Widget = ({ _id, title, intro }) => {
     }]
   }
 
-  const onMount = ref => {
-    if (ref) {
-      const rect = ref.parentElement.getBoundingClientRect()
-      setWidth(Math.min(rect.width, 425))
-    }
-  }
-
-  return <Wrapper open={open} ref={onMount}>
+  return <Wrapper open={open}>
     <Bubble onClick={() => setOpen(true)}>
       <span>{bubble.text}</span>
       {bubble.sources.length > 0
@@ -48,8 +33,10 @@ const Widget = ({ _id, title, intro }) => {
         </video>
         : null}
     </Bubble>
+    <Iframe>
     <Close onClick={() => setOpen(false)}>&times;</Close>
-    <Iframe {...iframe} />
+      <iframe {...iframe} />
+    </Iframe>
   </Wrapper>
 }
 
