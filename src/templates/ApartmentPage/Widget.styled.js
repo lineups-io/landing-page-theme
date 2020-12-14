cfb19f1ec5406b0901bc70dfebd68b204954972d
Lucas Blancas <lablancas@gmail.com>
2020-12-14 15:37:20 -0600

    fix: 🐛 add border radius to "Let's Tour" span element

--- src/templates/ApartmentPage/Widget.styled.js
+++ src/templates/ApartmentPage/Widget.styled.js
@@ -36,6 +36,7 @@ export const Bubble = styled.button`
     z-index: 1;
     width: 100%;
     height: 100%;
+    border-radius: 50%;
     display: flex;
     justify-content: center;
     align-items: center;
