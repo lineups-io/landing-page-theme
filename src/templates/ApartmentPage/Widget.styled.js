3275de081c79a51832baa8b48492b49f1d6c091f
Lucas Blancas <lablancas@gmail.com>
2020-12-14 12:11:49 -0600

    fix: 🐛 video overflowing outside bubble

--- src/templates/ApartmentPage/Widget.styled.js
+++ src/templates/ApartmentPage/Widget.styled.js
@@ -40,12 +40,11 @@ export const Bubble = styled.button`
   }
 
   video {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
     width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    object-fit: cover;
+    object-position: 50% 50%;
   }
 `
 
