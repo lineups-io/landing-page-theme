644dabef0c2e766c876d1dabbedb00817a459fc8
Lucas Blancas <lablancas@gmail.com>
2020-12-17 16:12:25 -0600

    refactor: 💡 show yellow apartment pin when highlighted

--- src/gatsby-theme-atomic-design/atoms/Map/ApartmentButton.js
+++ src/gatsby-theme-atomic-design/atoms/Map/ApartmentButton.js
@@ -3,10 +3,10 @@ import styled from 'styled-components'
 import ApartmentButton from 'gatsby-theme-atomic-design/src/atoms/Map/ApartmentButton'
 
 const CustomApartmentButton = styled(ApartmentButton)`
-  color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors[props.highlight ? 'tertiary' : 'primary']};
 
   svg {
-    stroke: ${props => props.theme.fontColorWithBackground.primary};
+    stroke ${props => props.theme.fontColorWithBackground[props.highlight ? 'tertiary' : 'primary']};
   }
 `
 
