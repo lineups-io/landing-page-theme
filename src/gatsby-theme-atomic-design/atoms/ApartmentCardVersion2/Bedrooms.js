ab773e6cacd10449dab6df3237f3f63be1b6818b
Lucas Blancas <lablancas@gmail.com>
2020-12-21 16:38:11 -0600

    refactor: 💡 update pricing text on card
    [gatsby-theme-atomic-design@2.17.8](https://github.com/lineups-io/gatsby-theme-atomic-design/compare/v2.17.7...v2.17.8) (2020-12-21)

---
+++ src/gatsby-theme-atomic-design/atoms/ApartmentCardVersion2/Bedrooms.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import Bedrooms from 'gatsby-theme-atomic-design/src/atoms/ApartmentCardVersion2/Bedrooms'
+
+export default props =>
+  <Bedrooms customText='Private bedrooms starting around' {...props} />
