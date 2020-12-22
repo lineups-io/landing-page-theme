e46021c9eb8a7273d405363a6bceec2038d9c423
Lucas Blancas <lablancas@gmail.com>
2020-12-22 16:28:33 -0600

    fix: 🐛 shorten text to prevent wrapping and text clipping

--- src/gatsby-theme-atomic-design/atoms/ApartmentCardVersion2/Bedrooms.js
+++ src/gatsby-theme-atomic-design/atoms/ApartmentCardVersion2/Bedrooms.js
@@ -3,4 +3,4 @@ import React from 'react'
 import Bedrooms from 'gatsby-theme-atomic-design/src/atoms/ApartmentCardVersion2/Bedrooms'
 
 export default props =>
-  <Bedrooms customText='Private bedrooms starting around' {...props} />
+  <Bedrooms customText='Private bedrooms starting at' {...props} />
--- src/gatsby-theme-atomic-design/atoms/ApartmentCardVersion2/Price.js
+++ src/gatsby-theme-atomic-design/atoms/ApartmentCardVersion2/Price.js
@@ -3,5 +3,5 @@ import React from 'react'
 import Price from 'gatsby-theme-atomic-design/src/atoms/ApartmentCardVersion2/Price'
 
 export default props =>
-  <Price customText='Private bedrooms starting around' {...props} />
+  <Price customText='Private bedrooms starting at' {...props} />
 
