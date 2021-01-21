94166ae2c0ccd5fd6895b9409a21b40f4f58f469
Lucas Blancas <lablancas@gmail.com>
2021-01-21 10:44:28 -0600

    feat: 🎸 new guest card and tour request email templates

---
+++ functions/send-tour-request-alert/index.js
@@ -0,0 +1,91 @@
+const dayjs = require('dayjs')
+const client = require('@sendgrid/client')
+
+client.setApiKey(process.env.SENDGRID_API_KEY)
+
+exports.handler = async function(event, context) {
+  // Only allow POST
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
+
+  const {
+    emailCc,
+    user: {
+      firstName: first_name,
+      lastName: last_name,
+      email,
+      phone: cell_phone,
+    },
+    notes,
+    apartment: {
+      name: apartment_name,
+    },
+    bedrooms,
+    ['move-in']: desired_move_in,
+    ['schedule-tour']: {
+      day,
+      time,
+    } = {},
+    ['contact-us']: {
+      question,
+    } = {},
+    ['floorplan-amenities']: floorplanAmenities,
+    ['community-amenities']: communityAmenities,
+    ['neighborhood-features']: neighborhoodFeatures,
+  } = JSON.parse(event.body)
+
+  const [desired_bedrooms] = bedrooms || []
+
+  const comments = [
+    `${first_name} is looking for a ${desired_bedrooms} around ${dayjs(desired_move_in).format('MM/DD/YYYY')}`,
+    `FLOORPLAN AMENITIES: ${ floorplanAmenities.join(', ') }`,
+    `COMMUNITY AMENITIES: ${ communityAmenities.join(', ') }`,
+    `NEIGHBORHOOD ACTIVITIES: ${ neighborhoodFeatures.join(', ') }`,
+  ]
+  if (notes) comments.splice(0, 0, `${ notes }\n--------------`)
+
+  // TODO: make template_id an environment variable ???
+  const template_id = 'd-a565492b18394500b1763d1de5b4ae7c'
+
+  const tour_date = day ? dayjs(day).format('MM/DD/YYYY') : ''
+  const tour_start_time = tour_date && time ? dayjs(`${ tour_date } ${ time }`).format('hh:mm a') : ''
+  const tour_end_time = tour_date && time ? dayjs(`${ tour_date } ${ time }`).add(30, 'minute').format('hh:mm a') : ''
+
+  const dynamic_template_data = {
+    apartment_name,
+    first_name,
+    last_name,
+    email,
+    cell_phone,
+    desired_move_in: desired_move_in && dayjs(desired_move_in).format('MM/DD/YYYY'),
+    desired_bedrooms,
+    apartment_tour: tour_date ? 'Apartment Tour' : '',
+    tour_date,
+    tour_start_time,
+    tour_end_time,
+    comments: comments.join('\n\n'),
+    floorplan: { options: floorplanAmenities.map(name => ({ name })) },
+    community: { options: communityAmenities.map(name => ({ name })) },
+  }
+
+  const to = emailCc ? emailCc.split(',').map(email => ({ email })) : undefined
+  if (!to) return
+
+  // TODO: make from email an environment variable
+  const body = {
+    from: { email: 'hi@lineups.io' },
+    personalizations: [{ to, dynamic_template_data }],
+    subject: 'You should not see this subject',
+    template_id,
+    content: [{ type: 'text/plain', value: 'You should not see this' }],
+  }
+
+  const request = {
+    method: 'POST',
+    url: '/v3/mail/send',
+    body,
+  }
+
+  return client.request(request).then(([response]) => response)
+}
