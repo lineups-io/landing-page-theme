aa86ee95bb49937811008de2c1fd94b6e2be8235
Lucas Blancas <lablancas@gmail.com>
2021-01-25 16:01:34 -0600

    feat: 🎸 show virtual tours when available

--- src/gatsby-theme-atomic-design/organisms/Sections/VirtualTours.js
+++ src/gatsby-theme-atomic-design/organisms/Sections/VirtualTours.js
@@ -1,3 +1,5 @@
 import React from 'react'
 
-export default () => <div />
+import VirtualTours from 'gatsby-theme-atomic-design/src/organisms/Sections/VirtualTours'
+
+export default props => props.tours && props.tours.length > 0 ? <VirtualTours {...props} /> : null
