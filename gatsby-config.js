056521428b3c06e2361586420a031e2c3422b464
Lucas Blancas <lablancas@gmail.com>
2021-02-03 11:19:20 -0600

    fix: 🐛 set content security policy for frame ancestors

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,7 @@ module.exports = {
         headers: {
           '/widgets/*': [
             'X-Frame-Options: SAMEORIGIN',
+            `Content-Security-Policy: frame-ancestors 'self' https://microsite-poc.webflow.io`,
             'Link: <https://cdn.filestackcontent.com>; rel=preconnect;',
             'Link: <https://res.cloudinary.com>; rel=preconnect;',
           ],
