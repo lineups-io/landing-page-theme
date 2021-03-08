0aaca1faac33110a25e3c15673faebc49a730dae
Lucas Blancas <lablancas@gmail.com>
2021-03-08 17:43:52 -0600

    build(gatsby v3 and upgrades): upgraded to gatsby v3 and all node dependencies

--- gatsby-node.js
+++ gatsby-node.js
@@ -148,3 +148,14 @@ exports.createPages = ({ graphql, actions }) => {
     )
   })
 }
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+      },
+    },
+  })
+}
