c7f79059f878f9fe718f6d574106b77829d9e442
Lucas Blancas <lablancas@gmail.com>
2021-03-09 01:10:18 -0600

    fix(ssr): handle undefined window for useLocalStorage hook

--- src/templates/WidgetPage/useLocalStorage.js
+++ src/templates/WidgetPage/useLocalStorage.js
@@ -7,6 +7,9 @@ const useLocalStorage = (key, initialValue) => {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === 'undefined')
+      return initialValue
+
     try {
       // Get from local storage by key
       const item = window.localStorage.getItem(namespacedKey);
