93b0be006cd04db6501b04d955ae81d2147c7fc7
Lucas Blancas <lablancas@gmail.com>
2021-03-09 16:08:50 -0600

    fix(routes email hash): use sha.js instead of native crypto

--- gatsby-node.js
+++ gatsby-node.js
@@ -148,14 +148,3 @@ exports.createPages = ({ graphql, actions }) => {
     )
   })
 }
-
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      fallback: {
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-      },
-    },
-  })
-}
