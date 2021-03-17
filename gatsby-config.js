37346e54eb29f052f4af2738b355bcfb0c47d5f8
Lucas Blancas <lablancas@gmail.com>
2021-03-17 17:14:50 -0500

    Revert "fix(headers): remove x-frame-options header. using CSP instead"
    This reverts commit b8a1e056757af3e322f8baa9e8f3368a85672fde.

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,7 @@ module.exports = {
         ],
         headers: {
           '/widgets/*': [
+            'X-Frame-Options: SAMEORIGIN',
             `Content-Security-Policy: frame-ancestors 'self' https://microsite-poc.webflow.io https://www.hometourstory.com`,
             'Link: <https://cdn.filestackcontent.com>; rel=preconnect;',
             'Link: <https://res.cloudinary.com>; rel=preconnect;',
