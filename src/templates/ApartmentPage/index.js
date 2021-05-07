e03f72d45a4f3800a521d7ac59bf240797855708
Lucas Blancas <lablancas@gmail.com>
2021-05-07 18:22:33 -0500

    fix(apartment page query): remove unused fields: special start and end date

--- src/templates/ApartmentPage/index.js
+++ src/templates/ApartmentPage/index.js
@@ -249,8 +249,6 @@ export const query = graphql`
             title
             description
             footer
-            startDate
-            endDate
           }
         }
         nearbyCommunities: nearby(limit: 3) {
