fb028b7cc3f1d51416811bb8621a65604a842c82
Lucas Blancas <lablancas@gmail.com>
2021-05-10 22:50:55 -0500

    refactor(facebook domain verification): add meta tag facebook domain verification

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -10,6 +10,7 @@ const Page = ({ data, location }) => {
 
   return <>
     <Helmet title={title}>
+      <meta name='facebook-domain-verification' content='v04xt28oi2n5tdmy1xj01w0ug4wx6k' />
       {[
         { name: 'description', content: 'Find a Home' },
       ].map((props, i) => <meta key={i} {...props} />)}
