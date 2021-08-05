7f086fa65116da6a0e58c0b48a25bda7ebf1e8b6
Lucas Blancas <lablancas@gmail.com>
2021-08-05 09:56:45 -0500

    refactor(card special banner): use type attribute to customize special banner

--- src/gatsby-theme-atomic-design/atoms/ApartmentCardVersion2/SpecialTag.js
+++ src/gatsby-theme-atomic-design/atoms/ApartmentCardVersion2/SpecialTag.js
@@ -2,9 +2,10 @@ import styled from 'styled-components'
 
 import SpecialTag from 'gatsby-theme-atomic-design/src/atoms/ApartmentCardVersion2/SpecialTag'
 
-const CustomSpecialTag = styled(SpecialTag)`
-  background: ${props => props.theme.colors.secondary};
-  color: ${props => props.theme.fontColorWithBackground.secondary};
+const CustomSpecialTag = styled(SpecialTag).attrs({
+  type: 'secondary',
+})`
+
 `
 
 export default CustomSpecialTag
