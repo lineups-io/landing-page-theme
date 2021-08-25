6b3f806a74103fce12c9bad6eab5d0485faaf2a4
Lucas Blancas <lablancas@gmail.com>
2021-08-25 15:52:15 -0500

    fix(time format): use lowercase am/pm without space

--- functions/realpage-update-lead/index.js
+++ functions/realpage-update-lead/index.js
@@ -58,8 +58,8 @@ exports.handler = async function(event, context) {
   const template_id = 'd-e8d7372deb3b476695936950be5e9134'
 
   const tour_date = day ? dayjs(day).format('MM/DD/YYYY') : ''
-  const tour_start_time = tour_date && time ? dayjs(`${ tour_date } ${ time }`, 'MM/DD/YYYY hh:mm A').format('hh:mm a') : ''
-  const tour_end_time = tour_date && time ? dayjs(`${ tour_date } ${ time }`, 'MM/DD/YYYY hh:mm A').add(30, 'minute').format('hh:mm a') : ''
+  const tour_start_time = tour_date && time ? dayjs(`${ tour_date } ${ time }`, 'MM/DD/YYYY hh:mma').format('hh:mm a') : ''
+  const tour_end_time = tour_date && time ? dayjs(`${ tour_date } ${ time }`, 'MM/DD/YYYY hh:mma').add(30, 'minute').format('hh:mm a') : ''
 
   const dynamic_template_data = {
     apartment_name,
