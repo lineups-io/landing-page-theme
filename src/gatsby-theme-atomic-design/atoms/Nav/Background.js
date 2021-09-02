e490cb9caa49dca79ce6d671584245fdd651284b
Lucas Blancas <lablancas@gmail.com>
2021-09-02 16:42:27 -0500

    Revert "fix(nav link): move styles into nav background styled component"
    This reverts commit 3cdbf3795c887895aa78c19914645ffd411d49bc.

--- src/gatsby-theme-atomic-design/atoms/Nav/Background.js
+++ src/gatsby-theme-atomic-design/atoms/Nav/Background.js
@@ -1,18 +1,10 @@
 import styled from 'styled-components'
 
 import Background from 'gatsby-theme-atomic-design/src/atoms/Nav/Background'
-import Link from 'gatsby-theme-atomic-design/src/atoms/Nav/Link'
 
 const CustomBackground = styled(Background).attrs({
   type: 'white',
 })`
-  ${ Link } {
-    color: ${props => props.theme.fontColorWithBackground.white};
-    font-size: 1.2em;
-    letter-spacing: 1px;
-    font-weight: 500;
-    text-transform: none;
-  }
 `
 
 export default CustomBackground
