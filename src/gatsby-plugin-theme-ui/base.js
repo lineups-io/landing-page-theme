de233016867bacdfbd5d10af90691659a9adf4ac
Lucas Blancas <lablancas@gmail.com>
2021-10-01 11:03:01 -0500

    fix(theme ui): overshadow theme object. theme options not used by gatsty-theme-lineups

---
+++ src/gatsby-plugin-theme-ui/base.js
@@ -0,0 +1,35 @@
+export default {
+  styles: {
+    root: {
+      WebkitFontSmoothing: 'antialiased',
+      letterSpacing: '1px',
+    },
+  },
+  fonts: {
+    body: 'Inter, sans-serif',
+    heading: 'Inter, sans-serif',
+  },
+  text: {
+    heading: {
+      marginBottom: 3,
+    },
+    paragraph: {
+      marginBottom: 4,
+    },
+  },
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#ca3022',
+    secondary: '#ca3022',
+    muted: '#f6f6f6',
+  },
+  breakpoints: [
+    '768px',
+    '1024px',
+    '1280px',
+  ],
+  sizes: {
+    container: 1340,
+  },
+}
---
+++ src/gatsby-plugin-theme-ui/gallery.js
@@ -0,0 +1,18 @@
+export default {
+  links: {
+    gallery: {
+      width: ['100%', '150px', '150px'],
+      color: 'text',
+      textDecoration: 'none',
+      fontFamily: 'body',
+      fontSize: 2,
+      fontWeight: 'body',
+      '> div': {
+        boxShadow: 'rgb(0 0 0 / 18%) 0px 2px 4px'
+      },
+      '&:hover img': {
+        opacity: 0.95
+      }
+    }
+  }
+}
