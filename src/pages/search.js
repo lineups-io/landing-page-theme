19e36ca00e8396ed49884584a960ec37210defdd
Lucas Blancas <lablancas@gmail.com>
2021-11-18 16:18:39 -0600

    fix: 🐛 add algolia app id and search key to all page contexts

--- src/pages/search.js
+++ src/pages/search.js
@@ -5,13 +5,13 @@ import algoliasearch from 'algoliasearch/lite'
 import Helmet from 'gatsby-theme-atomic-design/src/organisms/Helmet'
 import Layout from 'gatsby-theme-atomic-design/src/templates/Search'
 
-const SearchPage = ({ data, location, navigate }) => {
+const SearchPage = ({ data, location, navigate, pageContext }) => {
   const title = 'Search'
   const trackingData = { title, page: location.pathname }
 
   const searchClient = algoliasearch(
-    process.env.GATSBY_ALGOLIA_APP_ID,
-    process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+    pageContext.algoliaAppId,
+    pageContext.algoliaSearchKey,
   )
 
   return <>
