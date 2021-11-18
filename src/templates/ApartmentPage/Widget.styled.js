16b272f2d92bb9765197fcf3d41df07b4899b39c
Lucas Blancas <lablancas@gmail.com>
2021-11-18 10:25:25 -0600

    fix: 🐛 fill height on mobile devices instead of aspect ratio

--- src/templates/ApartmentPage/Widget.styled.js
+++ src/templates/ApartmentPage/Widget.styled.js
@@ -73,16 +73,18 @@ export const Iframe = styled.div`
   background-color: #fff;
   overflow-y: scroll;
   -webkit-overflow-scrolling: touch;
+  height: 100%;
 
-  @media (max-width: 374.9px) {
-  }
-
-  &::before {
-    content: '';
-    display: block;
-    width: 100%;
+  @media (min-width: 768px) {
     height: 0;
-    padding-top: 177.78%;
+
+    &::before {
+      content: '';
+      display: block;
+      width: 100%;
+      height: 0;
+      padding-top: 177.78%;
+    }
   }
 
   iframe {
