49bd69ea359d0e0c0bec86067bf42f87ec8eb8c3
Lucas Blancas <lablancas@gmail.com>
2022-02-18 14:01:29 -0600

    feat: 🎸 save lead to our database

---
+++ functions/save-lead/index.js
@@ -0,0 +1,91 @@
+const request = require('request-promise-native')
+
+const {
+  GRAPHQL_API_URI = 'http://localhost:4000/dev/api/graphql',
+  GRAPHQL_API_KEY = '',
+} = process.env
+
+const query = `
+  fragment LeadFields on Lead {
+    _id
+    firstName
+    lastName
+    email
+    phone
+
+    vendor
+
+    propertyId
+    # property
+
+    leadId
+    # lead
+
+    # activity
+
+    apartmentId
+    # apartment
+    createdAt
+    updatedAt
+  }
+
+  mutation saveLead ($doc: LeadInput) {
+    insertLead(doc: $doc) {
+      ...LeadFields
+    }
+  }
+`
+
+exports.handler = async function(event, context) {
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    apartmentId,
+    propertyId,
+    crmData,
+    vendor,
+  } = JSON.parse(event.body)
+
+  const variables = {
+    doc: {
+      firstName,
+      lastName,
+      email,
+      phone,
+      apartmentId,
+      propertyId,
+
+      // leadId: ID
+      crmData,
+
+      apiSettings: {
+        vendor,
+        /*
+        uri: String
+        username: String
+        password: String
+        id: ID
+        */
+      },
+    },
+  }
+
+  return request.post(GRAPHQL_API_URI, {
+    headers: {
+      Authorization: `Bearer ${GRAPHQL_API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    })
+  }).then(body => {
+    return {
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      statusCode: 200,
+      body,
+    }
+  })
+}
