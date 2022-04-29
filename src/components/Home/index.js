a4cbb7080352ded429dbbe1cffe69961c67e5bca
Lucas Blancas <lablancas@gmail.com>
2022-04-29 10:37:18 -0700

    refactor: 💡 remove covid banner

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,11 +5,9 @@ import Links from './Links'
 import About from './About'
 import Team from './Team'
 import Markets from './Markets'
-import Banner from 'gatsby-theme-atomic-design/src/organisms/Banner'
 
 export default ({ markets }) =>
   <>
-    <Banner />
     <Hero />
     <Links />
     <About />
