c92fdbbab2a2647c5afda60d47d0befa0340690f
Lucas Blancas <lablancas@gmail.com>
2022-09-06 09:58:02 -0500

    fix: 🐛 copy nav link component to gatsby site repo

--- src/gatsby-theme-atomic-design/atoms/Nav/Link.js
+++ src/gatsby-theme-atomic-design/atoms/Nav/Link.js
@@ -1,8 +1,52 @@
 import styled from 'styled-components'
 
-import Link from 'gatsby-theme-atomic-design/src/atoms/Nav/Link'
+import Link from 'gatsby-theme-atomic-design/src/atoms/Link'
 
-const CustomLink = styled(Link)`
+const NavLink = styled(Link)`
+  text-transform: uppercase;
+  letter-spacing: 2.75px;
+  font-size: 0.8em;
+  font-weight: 500;
+  padding: 0;
+  color: ${props => props.theme.fontColorWithBackground.primary};
+
+  position: relative;
+
+  svg + span {
+    padding-left: ${props => (props.theme.gutter * 2) / 3}px;
+  }
+
+  &:hover,
+  &.active,
+  .active & {
+    color: ${props => props.theme.colors.tertiary};
+    text-decoration: none;
+  }
+
+  &::before {
+    content: '';
+    width: 0;
+    height: 3px;
+    background-color: ${props => props.theme.colors.tertiary};
+    position: absolute;
+    z-index: 2;
+    left: 0;
+    bottom: 12px;
+    opacity: 0;
+    visibility: hidden;
+    transition: all 0.3s ease-in-out 0s;
+  }
+
+  &:hover::before,
+  &.active::before,
+  .active &::before {
+    width: 100%;
+    opacity: 1;
+    visibility: visible;
+  }
+`
+
+const CustomLink = styled(NavLink)`
   color: ${props => props.theme.fontColorWithBackground.white};
   font-size: 1.2em;
   letter-spacing: 1px;
