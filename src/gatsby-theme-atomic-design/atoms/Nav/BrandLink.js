ea03d5e4f0c83c8dba1939a070ca4c3bdc89d9ca
Lucas Blancas <lablancas@gmail.com>
2022-11-15 13:33:44 -0600

    fix: 🐛 remove padding around logo in navbar

--- src/gatsby-theme-atomic-design/atoms/Nav/BrandLink.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-import BrandLink from 'gatsby-theme-atomic-design/src/atoms/Nav/BrandLink'
-
-const CustomBrandLink = styled(BrandLink)`
-  padding: 15px 0;
-`
-
-export default CustomBrandLink
